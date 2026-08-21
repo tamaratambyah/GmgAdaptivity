b83a683505c8cbaf09f407ec13e7c2df1db1966e
Tamara Tambyah <tamara.tambyah@monash.edu>
2026-08-21 15:50:05 +1000

    update to runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Test
 TESTCASE = get(ENV, "TESTCASE", "seq")
 
+# Sequential tests
+if TESTCASE ∈ ("all", "seq", "seq-darcy")
+   include("DarcyGMG/seq/runtests.jl")
+end
+
 # MPI tests
 if TESTCASE ∈ ("all", "mpi", "mpi-darcy")
    include("DarcyGMG/mpi/runtests.jl")
